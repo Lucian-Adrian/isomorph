0819410e04da8ace70fc90a440f50559268e4770
Gavril Lucian-Adrian <lucian-adrian.gavril@isa.utm.md>
2026-03-19 01:05:36 +0200

    Add website dir and configure GitHub Pages deployment to deploy built site into website/dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/isomorph/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'website/dist',
   },
   test: {
     environment: 'jsdom',
